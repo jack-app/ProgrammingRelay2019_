bf4aa0f09dfd4dc2793c726bdb54db39b837f74c
nakata yuki <nasutaro211@gmail.com>
2019-02-21 13:47:42 +0900

    形態素解析する最初の部分を作った

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -10,4 +10,21 @@ class Wiki < ApplicationRecord
     def link
         "/wikis/#{self.title}"
     end
+
+    def save_body(row_body)
+      real_body = row_body
+      real_body = add_link(real_body)
+      #real_body = 関数(real_body)でできるようにして
+      self.update(body: real_body)
+    end
+
+    private
+    def add_link(body)
+      nm.enum_parse(body).each{|n|
+        break if n.is_eos?
+        if n.feature.start_with("名詞") && Wiki.find_by(title: n.surface).present?
+          #ここで書き換えて
+        end
+      }
+    end
 end
