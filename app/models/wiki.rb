3031cb253f0ea432f4683e8090fd1368ce2faa1c
K-jun <poittyo091221@gmail.com>
2019-02-21 14:36:26 +0900

    Keijun Done!

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,17 +2,21 @@ class Wiki < ApplicationRecord
 
   validates :title, presence: true, uniqueness: true
   validates :body, presence: true
+  before_save :save_body
 
     def self.search_with(word)
       Wiki.where("title LIKE ?", "%#{word}%")
     end
 
     def link
-        "/wikis/#{self.title}"
+      if self.title
+        return "/wikis/#{URI.encode(self.title)}"
+      end
     end
 
-    def save_body(row_body)
-      real_body = row_body
+    def save_body
+      real_body = self.body
+      puts "fucking chiocolate jesus"
       real_body = add_link(real_body)
       #real_body = 関数(real_body)でできるようにして
       self.update(body: real_body)
@@ -20,11 +24,22 @@ class Wiki < ApplicationRecord
 
     private
     def add_link(body)
+      nm = Natto::MeCab.new
+      puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+      puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+      puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+      puts nm.enum_parse(body)
+      puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+      puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+      puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
       nm.enum_parse(body).each{|n|
         break if n.is_eos?
-        if n.feature.start_with("名詞") && Wiki.find_by(title: n.surface).present?
+        if n.feature.start_with?("名詞") && Wiki.find_by(title: n.surface).present?
+          # body = n.feature.start_with?("名詞") && Wiki.find_by(title: n.surface).present?
+          puts n
           #ここで書き換えて
         end
       }
+      return body
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :wikis , param: :title
-  post "wiki/search" => "wiki#search"
+  post "wiki/search" => "wikis#search"
+  get "/" => "wikis#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
