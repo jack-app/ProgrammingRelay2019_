1bf84fd29872fdeb03efd597b3a4b7a6b4db50b9
nakata yuki <nasutaro211@gmail.com>
2019-02-21 13:09:29 +0900

    search_withメソッドを完成させた

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,8 +1,9 @@
 class Wiki < ApplicationRecord
     def self.search_with(word)
+      Wiki.where("title LIKE ?", "%#{word}%")
     end
 
     def link
         "/wikis/#{self.title}"
-    end        
+    end
 end
